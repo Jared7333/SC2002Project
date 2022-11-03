9bb85a60c7bc3a89d64881788fb17d43f63a845e
Banned333 <92076429+Banned333@users.noreply.github.com>
2022-11-04 00:37:13 +0800

    add CinemaSeats and updated Cinema

---
+++ Cinema.java
@@ -0,0 +1,108 @@
+package movies;
+
+public class Cinema {
+	private int cinemaId;
+	private int status; // 1 means vacant, 0 means taken
+	private int seatCount = 50;
+	private static int layout[] = new int[100];
+
+	private static CinemaSeats[] seat = new CinemaSeats[50];
+	// private int availSeats;
+	// private int unavailSeats;
+	// private String cinemaLocation;
+	private String cinemaClass;
+
+	public Cinema(int cinemaId, int seatCount) {
+		this.cinemaId = cinemaId;
+		for (int i = 0; i < seatCount; i++) {
+			seat[i] = new CinemaSeats(i);
+		}
+	}
+
+	public int getCinemaId() {
+		return cinemaId;
+	}
+
+	public void setCinemaId(int cinemaId) {
+		this.cinemaId = cinemaId;
+	}
+
+	public int getSeatCount() {
+		return seatCount;
+	}
+
+	public void setSeatCount(int status) {
+		this.seatCount = seatCount;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getCinemaClass() {
+		return cinemaClass;
+	}
+
+	public void setCinemaClass(String cinemaClass) {
+		this.cinemaClass = cinemaClass;
+	}
+
+	public static void cinemaLayout(int seatId) {
+		int size = 50; // seatCount
+
+		int s = 1;
+
+//		int seatId = 26; // get from ticket class
+		// Assigning seat numbers
+		for (int i = 0; i < size; i++) {
+			layout[i] = s++;
+//			System.out.println(seat[seatId].isOccupied());
+		}
+
+		// Printing layout
+		System.out.println("             Screen             ");
+		System.out.println("--------------------------------");
+		System.out.print("|");
+		for (int i = 0; i < size; i++) {
+
+			if ((layout[i] == seatId) || (seat[i].isOccupied() == true)) {
+				layout[i] = 00; // 00 means taken
+			}
+
+			if (layout[i] < 10) {
+				System.out.print("0" + layout[i]);
+			} else {
+				System.out.print(layout[i]);
+
+			}
+
+			if ((i + 1) % 10 != 0 && (i + 1) % 5 == 0) {
+				System.out.print(" ");
+
+			} else {
+				System.out.print("|");
+			}
+
+			if ((i + 1) % 10 == 0 && i + 1 != 50) {
+				System.out.println();
+				System.out.print("|");
+			}
+
+		}
+		System.out.print("\n");
+	}
+
+	public void assignSeat(int seatId) {
+		if (seat[seatId].isOccupied() == true) {
+			System.out.println("Seat already assigned to a customer.");
+			return;
+		}
+		seat[seatId].assign(seatId);
+		System.out.println("Seat Assigned!");
+	}
+
+}
---
+++ CinemaSeats.java
@@ -0,0 +1,58 @@
+package movies;
+
+public class CinemaSeats {
+
+	private int seatId;
+	private boolean assigned;
+	private int customerId;
+
+	public CinemaSeats(int seat_id) {
+		seatId = seat_id;
+		this.assigned = false;
+		this.customerId = 0;
+	}
+
+	public int getSeatId() {
+		return seatId;
+	}
+
+//	public void setSeatId(int seatId) {
+//		this.seatId = seatId;
+//	}
+//	public boolean isAssigned() {
+//		return assigned;
+//	}
+	public void setAssigned(boolean assigned) {
+		this.assigned = assigned;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public boolean isOccupied() {
+		if (this.assigned == false) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	public void assign(int seat_iD) {
+//		this.customerId = cust_id;
+		this.seatId = seat_iD;
+		this.assigned = true;
+	}
+
+	public void unAssigned() {
+		this.customerId = 0;
+		this.assigned = false;
+
+	}
+
+}
