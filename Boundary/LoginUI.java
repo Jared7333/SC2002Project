776756bfa3b8da742d03d2a2c171a92f933ca523
KaiKarp <112186832+KaiKarp@users.noreply.github.com>
2022-11-09 04:42:53 +0800

    Add files via upload

---
+++ Boundary/LoginUI.java
@@ -0,0 +1,23 @@
+package Boundary;
+
+public class LoginUI {
+	public void customer() {//both customer and admin should login via here,
+		System.out.println("Login for Customer");
+		//insert login for customer here here
+		
+		
+		
+		//after successful verification return
+		return;
+	}
+	
+	public void admin() {//both customer and admin should login via here,
+		System.out.println("Login for Admin");
+		//insert login admin here
+		
+		
+		//after successful verification return
+		return;
+	}
+	
+}
