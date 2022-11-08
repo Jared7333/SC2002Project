9f27eec8c35128e7c5cbe8b372fee8e769e964cf
Banned333 <92076429+Banned333@users.noreply.github.com>
2022-11-09 05:23:42 +0800

    Print login UI

--- Boundary/LoginUI.java
+++ Boundary/LoginUI.java
@@ -1,23 +1,48 @@
-package Boundary;
+import java.util.Scanner;
 
 public class LoginUI {
-	public void customer() {//both customer and admin should login via here,
+	public static boolean customer() {// both customer and admin should login via here,
+		System.out.println("Programmer Note: Choose ID: 1 ~ 100");
+		System.out.println("Programmer Note: Password: customerPASSWORD");
 		System.out.println("Login for Customer");
-		//insert login for customer here here
-		
-		
-		
-		//after successful verification return
-		return;
+		// insert login for customer here here
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter your User ID");
+			int UserID = sc.nextInt();
+			sc.nextLine();
+
+			System.out.println("Enter your Password");
+			String Password = sc.nextLine();
+
+			if ((UserID < 101 && UserID > 0) && Password.equals("customerPASSWORD")) {
+				System.out.println("Login successful");
+				return true;
+
+			} else {
+				System.out.println("Invalid UserID or Password");
+				System.out.println("1. Try again.");
+				System.out.println("Enter any number to exit Login Page.");
+				int choice = sc.nextInt();
+				if (choice == 1) {
+					continue;
+				} else {
+					break;
+				}
+			}
+
+		}
+
+		// after successful verification return
+		return false;
 	}
-	
-	public void admin() {//both customer and admin should login via here,
+
+	public void admin() {// both customer and admin should login via here,
 		System.out.println("Login for Admin");
-		//insert login admin here
-		
-		
-		//after successful verification return
+		// insert login admin here
+
+		// after successful verification return
 		return;
 	}
-	
+
 }
--- Boundary/MainMenuUI.java
+++ Boundary/MainMenuUI.java
@@ -1,29 +1,36 @@
-package Boundary;
 
 //import Body.*;
 import java.util.Scanner;
 
 public class MainMenuUI {
 	public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        boolean loop = true;
-        String accType = null;
-        System.out.println("MOBLIMA");
-        LoginUI login= new LoginUI();
-        while(loop){
-            System.out.println("(1) Customer or (2) Admin:");
-            accType = sc.nextLine();
-            accType.toLowerCase();
-            if(accType.equals("customer") || accType.equals("admin") || accType.equals("1") || accType.equals("2")){
-                loop = false;
-            }
-        }
-        
-        if (accType.equals("1") || accType.equals("customer")) {
-        	//call CustomerLogin?
-        }
-        else {
-        	//call AdminLogin?
-        }
+		Scanner sc = new Scanner(System.in);
+		boolean loop = true;
+		String accType = null;
+		System.out.println("MOBLIMA");
+		LoginUI login = new LoginUI();
+		while (loop) {
+			System.out.println("(1) Customer or (2) Admin:");
+			accType = sc.nextLine();
+			accType.toLowerCase();
+			if (accType.equals("customer") || accType.equals("admin") || accType.equals("1") || accType.equals("2")) {
+				loop = false;
+			}
+
+			if (accType.equals("1") || accType.equals("customer")) {
+				// call CustomerLogin?
+				boolean loginStatus = LoginUI.customer();
+				if (loginStatus == true) {
+					System.out.println("Will enter Customer UI stuffss");
+				} else if (loginStatus == false) {
+					loop = true;
+					continue;
+
+				} else {
+					// call AdminLogin?
+				}
+			}
+
+		}
 	}
 }
