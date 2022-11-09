23f7836567cb0c2b122abf4d65c827845bb34136
KaiKarp <112186832+KaiKarp@users.noreply.github.com>
2022-11-09 19:29:00 +0800

    Added implementation
    Calls CustomerUI or AdminUI depending on input

--- Boundary/MainMenuUI.java
+++ Boundary/MainMenuUI.java
@@ -1,36 +1,40 @@
+package Boundary;
 
+import java.io.IOException;
 //import Body.*;
 import java.util.Scanner;
 
 public class MainMenuUI {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException, ClassNotFoundException {
 		Scanner sc = new Scanner(System.in);
-		boolean loop = true;
-		String accType = null;
-		System.out.println("MOBLIMA");
+
 		LoginUI login = new LoginUI();
-		while (loop) {
-			System.out.println("(1) Customer or (2) Admin:");
-			accType = sc.nextLine();
-			accType.toLowerCase();
-			if (accType.equals("customer") || accType.equals("admin") || accType.equals("1") || accType.equals("2")) {
-				loop = false;
-			}
-
-			if (accType.equals("1") || accType.equals("customer")) {
-				// call CustomerLogin?
-				boolean loginStatus = LoginUI.customer();
-				if (loginStatus == true) {
-					System.out.println("Will enter Customer UI stuffss");
-				} else if (loginStatus == false) {
-					loop = true;
-					continue;
-
-				} else {
-					// call AdminLogin?
+		while (true) {
+			System.out.println("Welcome to MOBLIMA");
+			boolean loop = true;
+			String accType = null;
+			while (loop) {
+				System.out.println("(1) Customer,(2) Admin or (3) Exit MOBLIMA:");
+				accType = sc.nextLine();
+				accType.toLowerCase();
+				if (accType.equals("customer") || accType.equals("admin") || accType.equals("1") || accType.equals("2") || accType.equals("exit") || accType.equals("3")) {
+					loop = false;
 				}
 			}
 
+			if (accType.equals("1") || accType.equals("customer")) {
+				if (login.customer()) {
+					CustomerUI customer = new CustomerUI();
+					customer.main();
+				}
+			} else if (accType.equals("2") || accType.equals("admin")) {
+				login.admin();
+				AdminUI admin = new AdminUI();
+				admin.main();
+			}
+			else 
+				break;
+			
 		}
 	}
 }
