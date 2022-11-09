e5b49116dfaa57ae6e4b03a4bec619935249751c
KaiKarp <112186832+KaiKarp@users.noreply.github.com>
2022-11-10 03:43:21 +0800

    The start of MOBLIMA

---
+++ mainApp.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+import Boundary.MainMenuUI;
+public class mainApp {
+	public static void main(String args[]) throws ClassNotFoundException, IOException {
+		MainMenuUI mm = new MainMenuUI();
+		mm.main();
+	}
+}
