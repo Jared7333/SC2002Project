9c05ebd0e2c363fbf3b4e39f9c4aabb8831581fb
Jared7333 <jaredchansy@gmail.com>
2022-11-12 17:13:42 +0800

    javadocs for adminMethods

--- Body/adminMethods.java
+++ Body/adminMethods.java
@@ -2,8 +2,18 @@ package Body;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * Utility methods for the Admin class.
+ */
 public class adminMethods {
+    /**
+     * Creates a new Admin with a given name, ID, and password, and 
+     * updates the list of admins with the new Admin. 
+     * @param name the new Admin's name.
+     * @param loginID the new Admin's ID.
+     * @param password the password the new Admin uses to login.
+     * @param adminList the list of admins to be updated.
+     */
     public static void newAdmin(String name, String loginID, String password, ArrayList<Admin> adminList){
         for(Admin a : adminList){
             if (a.getLoginID() != null && a.getLoginID().matches(loginID)) {
@@ -15,7 +25,11 @@ public class adminMethods {
         adminList.add(create);
         System.out.println("Admin Account Created Successfully");
     }
-
+    /**
+     * Changes the password of an existing Admin.
+     * @param adminList list of current admins.
+     * @param current admin whose password is to be changed.
+     */
     public static void changePassword(ArrayList<Admin> adminList, Admin current){
         Scanner sc = new Scanner(System.in);
         System.out.printf("Current Password: %s \n", current.getPassword());
