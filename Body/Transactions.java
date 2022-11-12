dd677d309a5f55039a838bb669bde2c0b2b8d755
Banned333 <92076429+Banned333@users.noreply.github.com>
2022-11-13 01:20:04 +0800

    Fixed problem when rating 10 is being on as rating 1

--- Body/Transactions.java
+++ Body/Transactions.java
@@ -4,44 +4,20 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Class that handles the action of a customer buying a ticket.
- * Creates and contains the list of customers.
- * Also contains related methods such as displaying 
- * displaying a customer's booking history,
- * and updating a movie's rating after a customer has
- * rated it.
- */
 abstract public class Transactions {
 	static ArrayList<Customer> customerList = new ArrayList();
 	private int arrayCount = 0;
 
-	/**
-	 * Create list of customers.
-	 */
 	public Transactions() {
 		customerList = new ArrayList<Customer>();
 	}
 
-	/**
-	 * Display a customer's booking history.
-	 * @param iD the cutomer's ID.
-	 */
 	public void customerBookingHistory(int iD) {
 		customerList.get(iD).checkBookingHistory();
 	}
 
-	/**
-	 * Updates the customer's booking history and ticket ID.
-	 * Has the customer input their details if they
-	 * have not vought a ticket before.
-	 * @param iD the customer's ID.
-	 * @param movieTitle the movie's title.
-	 * @param chosenCineplex cineplex the customer has chosen.
-	 */
 	public void buyTicket(int iD, String movieTitle, int chosenCineplex) {
 		String Transact;
-
 		Scanner sc = new Scanner(System.in);
 		int phone;
 		int age;
@@ -53,7 +29,8 @@ abstract public class Transactions {
 //				customerList.add(c);
 //			}
 //		}
-		if (customerList.get(iD).customerBoughtBefore()) { // check if customer has bought ticket before, if
+
+		if (customerList.get(iD).customerBoughtBefore() == true) { // check if customer has bought ticket before, if
 																	// yes, skip the personal details
 			System.out.println("Obtaining details...");
 		} else {
@@ -99,24 +76,16 @@ abstract public class Transactions {
 		System.out.println("Customer's Phone Number: " + customerList.get(iD).getPhoneNo());
 		System.out.println("Customer's Email: " + customerList.get(iD).getEmail());
 		System.out.println("Customer's Age: " + customerList.get(iD).getAge());
-		customerList.get(iD).pastMoviesTitles.add(movieTitle); 
-		customerList.get(iD).pastTID.add(Transact = TransactionID.generateTID(iD, movieTitle)); 
+		customerList.get(iD).pastMoviesTitles.add(movieTitle); // update customer's watch history
+		customerList.get(iD).pastTID.add(Transact = TransactionID.generateTID(iD, movieTitle)); // update Customer's TID
 		customerList.get(iD).pastCineplex.add(chosenCineplex);
 
 	}
 
-	/**
-	 * Get customer's age.
-	 * @param iD customer's ID.
-	 * @return customer's age.
-	 */
 	public int getAge(int iD) {
 		return customerList.get(iD).getAge();
 	};
-	/**
-	 * Adds new customers to the customer list and updates their ID.
-	 * @param iD the customer's ID.
-	 */
+
 	public void setIDForFirstTimeUser(int iD) {
 		if (arrayCount < iD) { // create new element each time there is new iD
 
@@ -128,16 +97,16 @@ abstract public class Transactions {
 		customerList.get(iD).setID(iD);
 	}
 
-	/**
-	 * Updates a movie's rating from a customer's rating.
-	 * @param movieTitle the movie's title.
-	 * @param iD the cutomer's ID.
-	 * @param movieID the movie's ID.
-	 * @param movieList the current list of movies.
-	 */
 	public static void rateMovie(String movieTitle, int iD, int movieID, ArrayList<Movie> movieList) {
+		String ratingInString = "0";
 		String ratingAndReview = customerList.get(iD).rankMovies(movieTitle);
-		String ratingInString = ratingAndReview.substring(0, 1);
+		String ifRatingIsTen = ratingAndReview.substring(0, 2);
+		if (ifRatingIsTen.equals("10")) {
+			int rating = Integer.parseInt(ifRatingIsTen);
+			movieList.get(movieID).calOverallRating(rating);
+			return;
+		}
+		ratingInString = ratingAndReview.substring(0, 1);
 		int rating = Integer.parseInt(ratingInString);
 		movieList.get(movieID).calOverallRating(rating);
 	}
