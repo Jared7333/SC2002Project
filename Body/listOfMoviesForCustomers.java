d8326476c85bc2b268a293b9d4b337c923ed69c6
Banned333 <92076429+Banned333@users.noreply.github.com>
2022-11-13 01:59:56 +0800

    added total ratings beside movies in browse movies

--- Body/listOfMoviesForCustomers.java
+++ Body/listOfMoviesForCustomers.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class listOfMoviesForCustomers {
 	public static void browseMovies(ArrayList<Movie> movieList) throws IOException, ClassNotFoundException {
+		int totalRating = 0;
 
 //		newMovieList = s.importMovie(filenameMovie);
 		System.out.println("Movie List: ");
@@ -15,13 +16,29 @@ public class listOfMoviesForCustomers {
 
 		for (int i = 0; i < movieList.size(); i++) {
 			if (movieList.get(i).getStatus().equals("End Of Showing")) {
-				System.out.println(movieList.get(i).getName() + ": End Of Showing");
+				System.out.print(movieList.get(i).getName() + ": End Of Showing");
+
 			} else if (movieList.get(i).getStatus().equals("Coming Soon")) {
 				System.out.println(movieList.get(i).getName() + ": Coming Soon!");
 			} else if (movieList.get(i).getStatus().equals("Preview")) {
 				System.out.println(movieList.get(i).getName() + ": Preview");
+				for (int j = 0; j < movieList.get(i).getPastReview().size(); j++) {
+					totalRating += movieList.get(i).getPastReview().get(j);
+				}
+				totalRating = totalRating / movieList.get(i).getPastReview().size();
+
+				System.out.println(":  Total Rating:" + totalRating);
+				totalRating = 0;
 			} else {
-				System.out.println(movieList.get(i).getName());
+				System.out.print(movieList.get(i).getName());
+				for (int j = 0; j < movieList.get(i).getPastReview().size(); j++) {
+					totalRating += movieList.get(i).getPastReview().get(j);
+				}
+				totalRating = totalRating / movieList.get(i).getPastReview().size();
+
+				System.out.println(":  Total Rating:" + totalRating);
+				totalRating = 0;
+
 			}
 		}
 		System.out.println();
