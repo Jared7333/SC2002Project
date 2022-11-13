a27996cce0fcae3b7820a907ebce0cced40a1b0f
Banned333 <92076429+Banned333@users.noreply.github.com>
2022-11-13 23:14:47 +0800

    Update javadoc

--- mainApp.java
+++ mainApp.java
@@ -1,7 +1,18 @@
 import java.io.IOException;
 
 import Boundary.MainMenuUI;
+
+/**
+ * The start of the app. Creates a main menu object and calls it
+ */
 public class mainApp {
+	/**
+	 * Creates Main Menu
+	 * 
+	 * @param args args.
+	 * @throws ClassNotFoundException Error Check.
+	 * @throws IOException            Error Check.
+	 */
 	public static void main(String args[]) throws ClassNotFoundException, IOException {
 		MainMenuUI mm = new MainMenuUI();
 		mm.main();
